2fb60629320efcfa811f5b9c9daf8f482d52c204
Manoj Gautam <manojit.gautam@gmail.com>
2019-08-01 22:47:18 +0545

    'header'

---
+++ csn/src/components/Footer.js
@@ -0,0 +1,27 @@
+import React, { Compontent } from 'react';
+
+export default Footer extends Footer {
+    render() {
+        return (
+            <footer class="page-footer font-small unique-color-dark pt-4">
+                <div class="container">
+                    <ul class="list-unstyled list-inline text-center py-2">
+                        <li class="list-inline-item">
+                            <h5 class="mb-1">Register for free</h5>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#!" class="btn btn-outline-white btn-rounded">Sign up!</a>
+                        </li>
+                    </ul>
+
+                </div>
+
+                <div class="footer-copyright text-center py-3">© 2018 codeschoolnepal.com.np:
+        <a href="codeschoolnepal.com.np"> MDBootstrap.com</a>
+                </div>
+            </footer>
+
+
+        );
+    }
+}
